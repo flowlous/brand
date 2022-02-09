9b26e30e895cbd89b44aa270a1f63e5765e7d721
Max Fierro <elmasse@gmail.com>
2022-02-09 11:12:03 -0300

    Fixed missing react imports.

--- src/brand-name.js
+++ src/brand-name.js
@@ -1,4 +1,5 @@
 
+import React from 'react'
 
 export function BrandName ({ className, ...props}) {
 
--- src/logo.js
+++ src/logo.js
@@ -1,4 +1,6 @@
 
+import React from 'react'
+
 import { BrandName } from './brand-name'
 import { Mark } from './mark'
 
